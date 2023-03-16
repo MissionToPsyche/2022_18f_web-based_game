9ef9bae0bde4e9090d427c5b242fb459b1045a82
Alex Wong <wongalex2@gmail.com>
2023-03-15 18:07:23 -0700

    created shop scene, and scene transitions

--- Assets/Scripts/UpgradeShopScripts/ShopNavigation.cs
+++ Assets/Scripts/UpgradeShopScripts/ShopNavigation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuSceneTransition : MonoBehaviour
+public class ShopNavigation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -21,7 +21,7 @@ public class MainMenuSceneTransition : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName: "MainGameScene");
     }
 
-    public void switchSceneToShop(){
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName: "ShopScene");
+    public void switchSceneToMainMenu(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName: "MainMenuScene");
     }
 }
